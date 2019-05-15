e1f44b83c8cf8f8d63be6accb412100f75d025cc
Richard Wong <ricwtk@gmail.com>
2019-05-15 16:59:47 +0800

    set game at click of scoreboard and tournament

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ var app = new Vue({
   el: "#app",
   data: {
     main_player: "",
-    games: [{
-    }],
+    game: {
+      first_player: "",
+      second_player: "",
+      moves: [],
+      result: {}
+    },
     players: {},
     scores: {},
     showing_scoreboard: false,
@@ -53,11 +57,35 @@ var app = new Vue({
             });
             this.$set(this.scores, k, row); 
           })
+          .catch(err => { console.error(err); });
       });
     },
     setScoreHover: function (m1, m2) {
       this.$set(this.score_hover_text, 'p1', m1);
       this.$set(this.score_hover_text, 'p2', m2);
+    },
+    showGame: function (m1, m2) {
+      this.main_player = m1;
+      this.setGame(m1, m2)
+        .then(() => { this.showing_scoreboard = false; });
+    },
+    setGame: function (m1, m2) {
+      let req = new Request(`./results/players/${m1}.json`);
+      return fetch(req)
+        .then(r => {
+          if (r.status == 200) return r.json();
+          else throw new Error (`Error fetching scores for ${m1}`);
+        })
+        .then(r => {
+          let game = r.filter(x => x.second_player == m2);
+          if (game.length > 0) {
+            this.$set(this.game, 'first_player', m1);
+            this.$set(this.game, 'second_player', m2);
+            this.$set(this.game, 'moves', game[0].moves);
+            this.$set(this.game, 'result', game[0].result);
+          }
+        })
+        .catch(err => { console.error(err); });
     }
   }
 })
